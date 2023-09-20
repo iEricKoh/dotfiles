cac06a17fa757ff2c6c358d8406d18b0cdd7d4f1
eric <akaerickoh@gmail.com>
2023-09-20 14:22:02 +0800

    fixed alpha

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -15,6 +15,23 @@ end
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
+  -- add nvim-ufo
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = "kevinhwang91/promise-async",
+    event = "BufReadPost",
+    opts = {},
+
+    init = function()
+      -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+      vim.keymap.set("n", "zR", function()
+        require("ufo").openAllFolds()
+      end)
+      vim.keymap.set("n", "zM", function()
+        require("ufo").closeAllFolds()
+      end)
+    end,
+  },
 
   -- disable trouble
   { "folke/trouble.nvim", enabled = false },
