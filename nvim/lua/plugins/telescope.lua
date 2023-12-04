e7fc4e24250a9918991d3e892db27b183091fec4
Eric <iEricKoh@users.noreply.github.com>
2023-12-04 17:05:24 +0800

    add replace with args

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -10,7 +10,7 @@ return {
       },
       {
         "<C-g>",
-        "<cmd>Telescope live_grep<cr>",
+        "<cmd>:lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
       },
       {
         "<C-n>",
@@ -46,10 +46,26 @@ return {
     "telescope.nvim",
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
+
       build = "make",
       config = function()
         require("telescope").load_extension("fzf")
       end,
     },
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        -- This will not install any breaking changes.
+        -- For major updates, this must be adjusted manually.
+        version = "^1.0.0",
+      },
+    },
+    config = function()
+      require("telescope").load_extension("live_grep_args")
+    end,
   },
 }
